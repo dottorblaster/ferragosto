15d55b6e7543d03af26ada72cfc30c6c671657b6
Alessio Biancalana <dottorblaster@gmail.com>
2016-08-14 18:01:44 +0200

    spaces to tabs

--- bin/ferragosto.js
+++ bin/ferragosto.js
@@ -5,32 +5,32 @@ const nopt = require('nopt'),
 	Fs = require('fs'),
 	Request = require('request'),
 	Chalk = require('chalk'),
-  Home = require('homedir')();
+	Home = require('homedir')();
 
 function fetch(cb) {
 	const RequestUrl = "http://chefacciamoaferragosto.it/wp-json/wp/v2/posts";
 
-  console.log(Chalk.yellow('Fetching...\n'));
+	console.log(Chalk.yellow('Fetching...\n'));
 
-  Request.get(RequestUrl, (err, response, body) => {
-    if (!err && response.statusCode == 200) {
-      Fs.writeFile(Home + '/.ferragosto.json', body, (err) => {
-        if (err) {
-          return console.log(Chalk.red('HTTP request failed! A Ferragosto s\'attaccamo popo ar ca...\n'));
-        }
-        if (!!cb) {
-          cb();
-        }
-      });
-    }
-  });
+	Request.get(RequestUrl, (err, response, body) => {
+		if (!err && response.statusCode == 200) {
+			Fs.writeFile(Home + '/.ferragosto.json', body, (err) => {
+				if (err) {
+					return console.log(Chalk.red('HTTP request failed! A Ferragosto s\'attaccamo popo ar ca...\n'));
+				}
+				if (!!cb) {
+					cb();
+				}
+			});
+		}
+	});
 }
 
 function display(cb) {
-  const set = require(Home + '/.ferragosto.json'),
-    i = Math.floor(Math.random() * set.length);
+	const set = require(Home + '/.ferragosto.json'),
+		i = Math.floor(Math.random() * set.length);
 
-  console.log(Chalk.cyan('Che facciamo a ferragosto?\n') + Chalk.green('- ' + set[i].content.plaintext + '\n'));
+	console.log(Chalk.cyan('Che facciamo a ferragosto?\n') + Chalk.green('- ' + set[i].content.plaintext + '\n'));
 
 	if (!!cb) {
 		cb();
